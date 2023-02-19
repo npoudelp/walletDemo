45ee712426a2724e3c9ceb5338011914db3ea512
doctorCrazy <npoudelp@gmail.com>
2023-02-19 19:44:53 +0545

    excess code removed

--- wallet.sql
+++ wallet.sql
@@ -36,15 +36,6 @@ CREATE TABLE `account` (
 -- Dumping data for table `account`
 --
 
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES
-(3,3,2894),
-(4,4,9010),
-(5,5,100);
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
-
 --
 -- Table structure for table `accountLog`
 --
@@ -72,34 +63,6 @@ CREATE TABLE `accountLog` (
 -- Dumping data for table `accountLog`
 --
 
-LOCK TABLES `accountLog` WRITE;
-/*!40000 ALTER TABLE `accountLog` DISABLE KEYS */;
-INSERT INTO `accountLog` VALUES
-(5,3,'Master','Niroj Poudel',1000,9000,'09-01-2023','11:52:26am','sent'),
-(6,4,'Master','Niroj Poudel',1000,1000,'09-01-2023','11:52:26am','received'),
-(7,3,'Master','niroj poudel',100,8900,'09-01-2023','02:00:34pm','sent'),
-(8,4,'Master','niroj poudel',100,1100,'09-01-2023','02:00:34pm','received'),
-(9,4,'Niroj Poudel','master',1100,0,'09-01-2023','02:02:18pm','sent'),
-(10,3,'Niroj Poudel','master',1100,10000,'09-01-2023','02:02:18pm','received'),
-(11,3,'Master','niroj poudel',100,9900,'09-01-2023','02:16:35pm','sent'),
-(12,4,'Master','niroj poudel',100,100,'09-01-2023','02:16:35pm','received'),
-(13,4,'Niroj Poudel','master',110,0,'09-01-2023','02:25:30pm','sent'),
-(14,3,'Niroj Poudel','master',110,10000,'09-01-2023','02:25:30pm','received'),
-(15,3,'Master','niroj poudel',10,9880,'09-01-2023','02:40:47pm','sent'),
-(16,4,'Master','niroj poudel',10,10,'09-01-2023','02:40:47pm','received'),
-(17,3,'Master','master',880,9000,'09-01-2023','02:47:21pm','sent'),
-(18,3,'Master','master',880,10760,'09-01-2023','02:47:21pm','received'),
-(19,3,'Master','master',1234,9526,'09-01-2023','02:49:42pm','sent'),
-(20,3,'Master','master',1234,11994,'09-01-2023','02:49:42pm','received'),
-(21,3,'Master','niroj poudel',10000,1994,'09-01-2023','02:51:49pm','sent'),
-(22,4,'Master','niroj poudel',10000,10010,'09-01-2023','02:51:49pm','received'),
-(23,3,'Master','kalyan',100,1894,'09-01-2023','03:10:15pm','sent'),
-(24,5,'Master','kalyan',100,100,'09-01-2023','03:10:15pm','received'),
-(25,4,'Niroj Poudel','master',1000,9010,'12-01-2023','07:49:12am','sent'),
-(26,3,'Niroj Poudel','master',1000,2894,'12-01-2023','07:49:12am','received');
-/*!40000 ALTER TABLE `accountLog` ENABLE KEYS */;
-UNLOCK TABLES;
-
 --
 -- Table structure for table `userLog`
 --
@@ -124,25 +87,6 @@ CREATE TABLE `userLog` (
 -- Dumping data for table `userLog`
 --
 
-LOCK TABLES `userLog` WRITE;
-/*!40000 ALTER TABLE `userLog` DISABLE KEYS */;
-INSERT INTO `userLog` VALUES
-(12,3,'Logged in with ip : 103.104.28.218','09-01-2023','11:39:27am','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(13,4,'Logged in with ip : 103.104.28.218','09-01-2023','11:46:01am','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(14,4,'Logged in with ip : 103.104.28.218','09-01-2023','11:50:16am','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(15,4,'Logged in with ip : 103.104.28.218','09-01-2023','11:50:20am','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(16,3,'Logged in with ip : 103.104.28.218','09-01-2023','11:51:36am','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(17,3,'Logged in with ip : 103.104.28.218','09-01-2023','02:00:12pm','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(18,4,'Logged in with ip : 103.104.28.218','09-01-2023','02:01:01pm','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(19,3,'Logged in with ip : 103.104.28.218','09-01-2023','02:49:25pm','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(20,4,'Logged in with ip : 103.104.28.218','09-01-2023','02:52:18pm','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(21,5,'Logged in with ip : 103.104.28.218','09-01-2023','03:09:47pm','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(22,3,'Logged in with ip : 103.104.28.218','09-01-2023','03:10:00pm','City: Ilām, continent: Asia, Country: Nepal, Province: Province 1'),
-(23,4,'Logged in with ip : ','12-01-2023','07:45:46am',''),
-(24,3,'Logged in with ip : ','12-01-2023','07:48:46am','');
-/*!40000 ALTER TABLE `userLog` ENABLE KEYS */;
-UNLOCK TABLES;
-
 --
 -- Table structure for table `users`
 --
@@ -165,14 +109,6 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES
-(3,'Master','Nepal','9812345678','master@master.com','$2y$10$47SHVvsH4mV7hKkhduZPy.2SQj2te8vUhNYw6Qh.u88b0FRUADneK'),
-(4,'Niroj Poudel','Urlabari','9862258058','npoudelp@gmail.com','$2y$10$WbNjxmZt6S8pMJKuOwKXAOyRKdyRXi7L8uuNBP8/wLDN1M/Py2LJ2'),
-(5,'kalyan','Ithari','9825341454','niro@gmail.com','$2y$10$quGKfIFIY3GNP08hjJvNrudE4YCmIq/eRkHzmYtWa21LXF0UbXr92');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
